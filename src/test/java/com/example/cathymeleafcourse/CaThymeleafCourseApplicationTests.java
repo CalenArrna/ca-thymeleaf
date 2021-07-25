bc0bdd29057ce0ac2d52ba75d494e842050ec11d
calenarrna <calenarrna@gmail.com>
2021-07-25 16:07:48 +0200

    initial commit of project after creating basic htmls.

---
+++ src/test/java/com/example/cathymeleafcourse/CaThymeleafCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cathymeleafcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaThymeleafCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
